13fb7403c567b599de99a992da0c292b4cbb9fa5
Leandro Sanches <leandro.sanches@efuzyon.com>
2016-05-06 18:14:32 -0300

    Changing views

--- dashboard/View/admin/menu/menu.blade.php
+++ dashboard/View/admin/menu/menu.blade.php
@@ -1,3 +1,5 @@
+{{ Html::Hook("Core::Admin::Template::Menu::Item::Before", "dashboard") }}
 <li>
     <a href="{{ URL::Route('hakon::dashboard') }}" class="waves-effect active"><i class="ti-home"></i> <span> Dashboard </span> </a>
 </li>
+{{ Html::Hook("Core::Admin::Template::Menu::Item::After", "dashboard") }}
